d8339132a211151dd81bec2499ffe79f3aff1c4d
Pritirekha23 <learnpriti23@gmail.com>
2023-11-13 19:42:03 +0530

    day2

--- todo02/todoapp/urls.py
+++ todo02/todoapp/urls.py
@@ -4,7 +4,5 @@ from .views import home
 
 urlpatterns = [
      path('home/',home,name='home'),
-     # path('index/',index,name='index'),
-     # path('add_todo/',add_todo,name='add_todo')
- 
+    
 ]
