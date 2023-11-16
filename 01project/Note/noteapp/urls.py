b263703a4755fa9a1e8b3e281157473b92f30b39
Pritirekha23 <learnpriti23@gmail.com>
2023-11-16 17:39:27 +0530

    django

--- 01project/Note/noteapp/urls.py
+++ 01project/Note/noteapp/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
    path('', home, name='home'),
    path('addtask/',addtask,name="addtask"),
    path('delete/<int:id>',delete, name="delete"),
-   path('update/<int:id>',update, name="update")
+   path('update/<int:id>/',update, name="update")
 ]
